85d90fca31585b806f1e6ce5c02adeb86a32c023
qyuiq <popow2281337@gmail.com>
2025-11-18 04:30:33 +0700

    result

--- app/src/main/java/com/example/roomdemo/ProductRepository.kt
+++ app/src/main/java/com/example/roomdemo/ProductRepository.kt
@@ -1,6 +1,5 @@
 package com.example.roomdemo
 
-import ProductDao
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.CoroutineScope
--- app/src/main/java/com/example/roomdemo/ProductRoomDatabase.kt
+++ app/src/main/java/com/example/roomdemo/ProductRoomDatabase.kt
@@ -1,8 +1,9 @@
+package com.example.roomdemo
+
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.roomdemo.Product
 
 @Database(entities = [(Product::class)], version = 1, exportSchema = false)
 abstract class ProductRoomDatabase: RoomDatabase() {
